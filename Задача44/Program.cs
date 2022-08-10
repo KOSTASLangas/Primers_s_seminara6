1f1690817a9524b3556f443769f7fe6cd1ab73a7
Kostas <langas.kostya@mail.ru>
2022-08-10 17:54:55 +0500

    Вывод n чисел фибоначчи (сколько ввел, столько раз и посчитал и вывел)

---
+++ Задача44/Program.cs
@@ -0,0 +1,11 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+int n = Convert.ToInt32(Console.ReadLine());
+int a0 = 0, a1 = 1;
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a0 + " ");
+    int x = a0+a1; 
+    a0 = a1;
+    a1 = x;
+
+}
---
+++ Задача45/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+    array[i] = new Random().Next(0, 101);
+
+int[] array1 = new int[array.Length];
+for (int i = 0; i < array1.Length; i++)
+{
+    int x = array[i];
+    array1[i] = x;
+}
+array1[0] = -10;
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("[" + string.Join(", ", array1) + "]");
