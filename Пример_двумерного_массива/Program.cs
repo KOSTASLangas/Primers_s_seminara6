2e64627bd7ae75a0d0d1ed585d434d9e61988116
Kostas <langas.kostya@mail.ru>
2022-08-10 15:22:28 +0500

    Шаблон создания двумерного массива с его выводом и заполнением

---
+++ Пример_двумерного_массива/Program.cs
@@ -0,0 +1,17 @@
+﻿// Заполнение двумерного массива с последующим его выводом
+
+int [,] array = new int [5,5];
+for (int i = 0; i < array.GetLength(0); i++) // цикл с 4 по 9 строки отвечает за заполнение массива
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        //array[i,j] = Convert.ToInt32(Console.ReadLine()); // ввод чисел для заполнения массива с клавиатуры
+        array [i, j] = new Random().Next (1, 11);  // ввод чисел для заполнения рандомом
+}
+
+for (int i = 0; i < array.GetLength(0); i++) // цикл с 11 по 16 строки отвечает за вывод всего массива в виде таблицы
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+
