cb0ef4c0f6f67ecdd02d41c421fdd877be09da66
Kostas <langas.kostya@mail.ru>
2022-08-10 16:05:15 +0500

    Переворот одномерного массива(запись задом на перед)

---
+++ Задача39/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+Console.WriteLine("Введите количество элементов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array [i] = new Random().Next(100, 1000);
+Console.WriteLine( "Начальный массив: [" + string.Join (", ", array) + "]");
+for (int i = 0; i < n / 2; i++)
+{
+    int x = array[i];
+    array [i] = array [n - i - 1];
+    array [n - i - 1] = x; 
+}
+Console.WriteLine( "Начальный массив: [" + string.Join (", ", array) + "]");
