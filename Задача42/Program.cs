6eb1638bb416272ccf4644c7bf662827d839f839
Kostas <langas.kostya@mail.ru>
2022-08-10 17:03:17 +0500

    Перевод в двоичную систему счисления

---
+++ Задача42/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+int n = Convert.ToInt32(Console.ReadLine());
+string end = "";
+while (n > 0 )
+{
+    int x =  n % 2;
+    end = Convert.ToString(x) + end; // положение "+end" регулирует положение цифры при выводе (начало/конец)
+    n /= 2;
+}
+Console.WriteLine(end);
+
+
