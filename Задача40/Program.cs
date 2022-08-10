fdb57fcb369b1a0f50769397651e497bd432fc21
Kostas <langas.kostya@mail.ru>
2022-08-10 16:29:31 +0500

    Теорема неравенства треугольника

---
+++ Задача40/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+Console.WriteLine("Введите длины сторон");
+
+int[] array = new int[3];
+for (int i = 0; i < 3; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "Начальный массив: [" + string.Join (", ", array) + "]");   
+
+if (array[0] < array[1] +  array [2] && array[1] < array[0] +  array [2] && array[2] < array[1] +  array [0])
+Console.WriteLine("Треугольник может существовать");
+else 
+Console.WriteLine("Треугольник не может существовать");
